8d5eb1bf9353cefea97beaaf02c405c3ea866cf1
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-21 19:10:24 +0800

    Update numberGuess.js

--- numberGuess.js
+++ numberGuess.js
@@ -4,7 +4,7 @@ const number = 5
 let guess;
 
 while (guess !== number) {
-// prompt that will ask the user to enter thier guess    
+// prompt that will ask the user to enter their guess    
     guess = parseInt(prompt(" guess the number from 1 to 10: "))
     
 //  it will determine if its is higher, lower or correct
